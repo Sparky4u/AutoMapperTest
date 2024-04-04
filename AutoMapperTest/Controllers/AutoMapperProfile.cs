44681a72b5070e52d89ffa2bbc196021c991eff2
Вадим <@DESKTOP-988LNTV>
2024-04-04 21:32:52 +0300

    Add project files.

---
+++ AutoMapperTest/Controllers/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using AutoMapperTest.Models;
+
+namespace AutoMapperTest.Controllers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile() 
+        {
+            CreateMap<SuperHero, SuperHeroDto>();
+            CreateMap<SuperHeroDto, SuperHero>();
+        }
+    }
+}
